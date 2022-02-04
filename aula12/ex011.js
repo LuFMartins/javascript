fc7c385393e8f29110558ffed87c71fe6b6a6a05
Luiz Gabriel F. Martins <94010799+LuFMartins@users.noreply.github.com>
2022-02-03 21:32:35 -0300

    Update aula12

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -1,8 +1,10 @@
-var idade = 59;
+var idade = 69;
+console.log(`Vocẽ tem ${idade} anos`);
+
 if(idade < 16){
   console.log('Não vota');
-} else if(idade >= 16 && idade < 18 || idade >= 60){
-  console.log('Voto opcional');
+} else if(idade < 18 || idade >= 65){
+    console.log('Voto opcional');
 }else{
   console.log('Voto obrigatorio');
 }
