89ad4e88dcc0c5657b6c25c2f5a9021c6f8b7db9
Luiz Gabriel F. Martins <94010799+LuFMartins@users.noreply.github.com>
2022-02-07 22:10:05 -0300

    ex014

--- aula12-ex/ex014/script.js
+++ aula12-ex/ex014/script.js
@@ -1,7 +1,20 @@
 function carregar(){
-  var msg = window.document.querySelector('div#msg');
-  var foto = window.document.querySelector('div#foto');
+  var msg = document.querySelector('div#msg');
+  var img = document.querySelector('img#imagem');
   var data = new Date();
   var hora = data.getHours();
   msg.innerHTML = `Agora são ${hora} horas`;
+  if(hora >= 0 && hora < 12){
+    img.src = "imagens/manha.png";
+    window.alert('Bom Dia!')
+    document.body.style.background = 'rgb(213, 196, 48)';
+  } else if(hora >= 12 && hora < 18){
+    img.src = "imagens/tarde.png";
+    window.alert('Boa Tarde!')
+    document.body.style.background = 'rgb(172, 99, 27)';
+  } else {
+    img.src = "imagens/noite.png";
+    document.body.style.background = 'rgb(27, 43, 90)';
+    window.alert('Boa Noite!');
+  }
 }
