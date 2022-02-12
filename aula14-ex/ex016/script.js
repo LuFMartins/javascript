447bf49f3e00a96fbcfaacba9e68b865cc5a726f
Luiz Gabriel F. Martins <94010799+LuFMartins@users.noreply.github.com>
2022-02-12 20:01:49 -0300

    update - ex016

---
+++ aula14-ex/ex016/script.js
@@ -0,0 +1,10 @@
+function contar(){
+  var c = Number(document.querySelector('input#nmb1'));
+  var f = Number(document.querySelector('input#nmb2'));
+  var p = Number(document.querySelector('input#nmb3'));
+  var res = document.querySelector('div.res');
+
+  for(c; c <= f; c += p){
+    
+  }
+}
---
+++ aula14-ex/ex016/teste.js
@@ -0,0 +1,8 @@
+var comeco = 1
+var final = 10
+var contagem = 1
+
+
+for(comeco; comeco <= final; comeco += contagem){
+  console.log(comeco);
+}
