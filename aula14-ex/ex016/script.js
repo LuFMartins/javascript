90ae3e6f43f927967b9d001380de795894689445
Luiz Gabriel F. Martins <94010799+LuFMartins@users.noreply.github.com>
2022-02-12 23:35:08 -0300

    update - ex016

--- aula14-ex/ex016/script.js
+++ aula14-ex/ex016/script.js
@@ -1,10 +1,19 @@
 function contar(){
-  var c = Number(document.querySelector('input#nmb1'));
-  var f = Number(document.querySelector('input#nmb2'));
-  var p = Number(document.querySelector('input#nmb3'));
+  var começo = document.querySelector('input#nmb1');
+  var final = document.querySelector('input#nmb2');
+  var passo = document.querySelector('input#nmb3');
   var res = document.querySelector('div.res');
 
-  for(c; c <= f; c += p){
-    
+  if(começo.value.length == 0 || final.value.length == 0 || passo.value.length == 0){
+    alert('[ERRO] - FALTAM DADOS!');
+  } else {
+    res.innerHTML = 'Contando: '
+    var i = Number(começo.value);
+    var f = Number(final.value);
+    var p = Number(passo.value);
+
+    for(var c = i; i <= f; i += p) {
+      res.innerHTML += `${i} < `;
+    }
   }
 }
