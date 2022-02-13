cf30940a43624e7b0cd06c8318c36aa55a1fac31
Luiz Gabriel F. Martins <94010799+LuFMartins@users.noreply.github.com>
2022-02-13 14:56:12 -0300

    Update script.js

--- aula14-ex/ex016/script.js
+++ aula14-ex/ex016/script.js
@@ -6,14 +6,29 @@ function contar(){
 
   if(começo.value.length == 0 || final.value.length == 0 || passo.value.length == 0){
     alert('[ERRO] - FALTAM DADOS!');
+    res.innerHTML = 'POR FAVOR, PREENCHA TODAS AS CAIXAS!'
+    
   } else {
-    res.innerHTML = 'Contando: '
+    res.innerHTML = 'Contando: <br>'
     var i = Number(começo.value);
     var f = Number(final.value);
     var p = Number(passo.value);
 
-    for(var c = i; i <= f; i += p) {
-      res.innerHTML += `${i} < `;
+    if(p == 0){
+      alert('Passo 0 não existe, consideramos o passo 1');
+      p = 1;
     }
+
+    if( i < f ){
+      for(i; i <= f; i += p) {
+        res.innerHTML += `${i} - `;
+      }
+    } else{
+      for(i; i > f; i-=p){
+        res.innerHTML += `${i} - `;
+      }
+    }
+    
+    res.innerHTML += '*';
   }
 }
