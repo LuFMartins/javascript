cb650ceca4a28aa2a968db32385b0710223cfe5f
Luiz Gabriel F. Martins <94010799+LuFMartins@users.noreply.github.com>
2022-02-15 20:54:32 -0300

    Update script.js

--- aula14-ex/ex016/script.js
+++ aula14-ex/ex016/script.js
@@ -1,16 +1,16 @@
 function contar(){
-  var começo = document.querySelector('input#nmb1');
+  var comeco = document.querySelector('input#nmb1');
   var final = document.querySelector('input#nmb2');
   var passo = document.querySelector('input#nmb3');
   var res = document.querySelector('div.res');
 
-  if(começo.value.length == 0 || final.value.length == 0 || passo.value.length == 0){
+  if(comeco.value.length == 0 || final.value.length == 0 || passo.value.length == 0){
     alert('[ERRO] - FALTAM DADOS!');
     res.innerHTML = 'POR FAVOR, PREENCHA TODAS AS CAIXAS!'
     
   } else {
     res.innerHTML = 'Contando: <br>'
-    var i = Number(começo.value);
+    var i = Number(comeco.value);
     var f = Number(final.value);
     var p = Number(passo.value);
 
@@ -21,14 +21,15 @@ function contar(){
 
     if( i < f ){
       for(i; i <= f; i += p) {
-        res.innerHTML += `${i} - `;
+        res.innerHTML += `${i}`;
+        if((i + p) <= f) res.innerHTML += ' - ';
       }
     } else{
       for(i; i > f; i-=p){
-        res.innerHTML += `${i} - `;
+        res.innerHTML += `${i} `;
+        if((i + p) >= f) res.innerHTML += ' - ';
       }
     }
     
-    res.innerHTML += '*';
   }
 }
