1336a58deea607977493fbe381758ec6f5588116
Luiz Gabriel F. Martins <94010799+LuFMartins@users.noreply.github.com>
2022-04-19 15:09:27 -0300

    Update vetornatela.js

--- aula15/vetornatela.js
+++ aula15/vetornatela.js
@@ -1,5 +1,8 @@
 let valores = [1,2,9,2,4,8];
 
-for (var p = 0; p < valores.length; p++){
-  console.log(valores[p]);
+for (var p in valores){
+  console.log(`a posição ${p} tem o valor ${valores[p]}`);
 }
+
+//achando valores dentro do array
+console.log(valores.indexOf(1));
