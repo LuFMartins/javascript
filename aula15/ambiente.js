461e306753243270f2c6216b7dc656ddc09dfce2
Luiz Gabriel F. Martins <94010799+LuFMartins@users.noreply.github.com>
2022-04-19 14:55:51 -0300

    create aula15

---
+++ aula15/ambiente.js
@@ -0,0 +1,9 @@
+let num = [8,7,1];
+num[3] = 9;
+num.push(5);
+num.sort();
+console.log(num.length);
+console.log(`o valor da nossa váriavel é ${num}`);
+
+
+
---
+++ aula15/vetornatela.js
@@ -0,0 +1,5 @@
+let valores = [1,2,9,2,4,8];
+
+for (var p = 0; p < valores.length; p++){
+  console.log(valores[p]);
+}
